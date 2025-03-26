4427e75322a9456d46fc0059ad985f62e9af7b7e
Tomáš Poledníček <polednicek.tomas@seznam.cz>
2025-03-26 10:23:58 +0100

    Fix

--- script.py
+++ script.py
@@ -1,6 +1,58 @@
 import os
 import pandas as pd
 from scipy.interpolate import RectBivariateSpline
+import logging
+import sys
+import warnings
+from argparse import ArgumentParser
+from logging.handlers import WatchedFileHandler
+from pathlib import Path
+
+logger = logging.getLogger("europa")
+
+
+def parse_args(argv):
+    parser = ArgumentParser(description="Invert EoS data from (T,mub) to (e,nb)")
+    parser.add_argument("--logfile", metavar="FILE", help="Log to a specified file")
+    parser.add_argument("--loglevel", metavar="LEVEL", default="INFO", help="Log level")
+
+    subparsers = parser.add_subparsers(
+        description="valid subcommands", dest="subcommand", required=True
+    )
+    parser_invert = subparsers.add_parser(
+        "invert", help="Invert from (T,mub) to (e,nb)"
+    )
+    parser_invert.add_argument(
+        "--output",
+        metavar="DIR",
+        required=True,
+        help="Output directory",
+    )
+
+    parser_invert.add_argument(
+        "--input",
+        metavar="DIR",
+        required=True,
+        help="Input directory",
+    )
+    args = parser.parse_args(argv)
+    return args
+
+
+def configure_logging(filename=None, level=logging.DEBUG):
+    if filename:
+        handler = WatchedFileHandler(filename, encoding="utf-8")
+    else:
+        handler = logging.StreamHandler(sys.stdout)
+    logging.basicConfig(
+        format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=level,
+        handlers=[handler],
+    )
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
+    logging.captureWarnings(True)
+    warnings.simplefilter("default")
 
 
 def open_all_files(directory):
@@ -9,7 +61,7 @@ def open_all_files(directory):
         for filename in os.listdir(directory):
             filepath = os.path.join(directory, filename)
             if os.path.isfile(filepath):
-                print(f"Opening {filename}")
+                logger.info(f"Opening {filename}")
                 if "EnerDens_Final_" in filename:
                     enerdens = pd.read_csv(
                         filepath, sep="\t", header=None, names=["T", "mub", "e"]
@@ -19,12 +71,12 @@ def open_all_files(directory):
                         filepath, sep="\t", header=None, names=["T", "mub", "nb"]
                     )
 
-        if not all([enerdens is not None, bardens is not None]):
+        if enerdens is None or bardens is None:
             raise ValueError("Missing required data files!")
 
         return enerdens, bardens
     except Exception as e:
-        print(f"Error: {e}")
+        logger.exception(f"Error: {e}")
         return None, None
 
 
@@ -41,11 +93,13 @@ def prepare_interpolators(enerdens, bardens):
     E_interp = RectBivariateSpline(T, mub, e_pivot.values)
     NB_interp = RectBivariateSpline(T, mub, nb_pivot.values)
 
+    logger.info("Interpolators were succesfully created ")
+
     return E_interp, NB_interp, T, mub
 
 
 def find_eos(E_interp, NB_interp, T_values, mub_values, e_value, nb_value):
-    print("Hledáme řešení pro e =", e_value, ", nb =", nb_value)
+    logger.info(f"looking for a solution for e = {e_value} a nb = {nb_value}")
 
     for mub in mub_values:
         T_found = None
@@ -84,31 +138,35 @@ def find_eos(E_interp, NB_interp, T_values, mub_values, e_value, nb_value):
                 break
 
         if T_found is not None and T_nb_found is not None:
-            print(f"mub = {mub}, T_found = {T_found}, T_nb_found = {T_nb_found}")
+            logger.info(f"mub = {mub}, T_found = {T_found}, T_nb_found = {T_nb_found}")
             if abs(T_nb_found - T_found) < 2.0:
-                print("Řešení nalezeno:", T_found, mub)
+                logger.info(f"Solution was found: {T_found} {mub}")
                 return T_found, mub
 
-    print("Žádné řešení nebylo nalezeno.")
+    logger.info("No solution was found.")
     return None
 
 
-def main():
-    directory = "/Users/tomaspolednicek/Desktop/EoS-script/data"
-    enerdens, bardens = open_all_files(directory)
-    E_interp, NB_interp, T, mub = prepare_interpolators(enerdens, bardens)
-    min = 0
-    max = 13
-    N = 400
-    dp = (max - min) / N
-    e_grid = []
-    nb_grid = []
+def main(argv):
+    args = parse_args(argv)
+    configure_logging(filename=args.logfile, level=args.loglevel)
 
-    for i in range(N):
-        e_grid.append(min + (i + 0.5) * dp)
-        nb_grid.append(min + (i + 0.5) * dp)
+    try:
+        directory = Path(args.input)
+        enerdens, bardens = open_all_files(directory)
+        E_interp, NB_interp, T, mub = prepare_interpolators(enerdens, bardens)
+        # min = 0
+        # max = 13
+        # N = 400
+        # dp = (max - min) / N
+        # e_grid = []
+        # nb_grid = []
 
-    if enerdens is not None and bardens is not None:
+        # for i in range(N):
+        #    e_grid.append(min + (i + 0.5) * dp)
+        #    nb_grid.append(min + (i + 0.5) * dp)
+
+        # if enerdens is not None and bardens is not None:
         # e_value = 0.1748480499691949
         # nb_value = 0.0000000000000700
         e_value = 11.68
@@ -123,6 +181,10 @@ def main():
         # else:
         #    print("No matching values found.")
 
+    except Exception:
+        logger.exception("Something went wrong.")
+
 
 if __name__ == "__main__":
-    main()
+    status = main(sys.argv[1:])
+    sys.exit(status)

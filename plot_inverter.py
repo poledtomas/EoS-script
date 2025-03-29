308dd5c36176acaeb4433676355ab42741012e6b
Tomáš Poledníček <polednicek.tomas@seznam.cz>
2025-03-29 21:07:14 +0100

    Fix plot_inverter and notebook

--- plot_inverter.py
+++ plot_inverter.py
@@ -7,24 +7,11 @@ from scipy.interpolate import griddata
 
 def open_all_files(directory):
     try:
-        press, enerdens, bardens = None, None, None
         for filename in os.listdir(directory):
             filepath = os.path.join(directory, filename)
             if os.path.isfile(filepath):
-                print(f"Opening {filename}")
-                if "Press_Final_PAR_" in filename:
-                    press = pd.read_csv(
-                        filepath, sep="\t", header=None, names=["mub", "T", "P"]
-                    )
-                elif "EnerDens_Final_" in filename:
-                    enerdens = pd.read_csv(
-                        filepath, sep="\t", header=None, names=["mub", "T", "e"]
-                    )
-                elif "BarDens_Final_" in filename:
-                    bardens = pd.read_csv(
-                        filepath, sep="\t", header=None, names=["mub", "T", "nb"]
-                    )
-                elif "inventer" in filename:
+                if "inventer" in filename:
+                    print(f"Opening {filename}")
                     inv = pd.read_csv(
                         filepath,
                         sep="\s+",
@@ -32,42 +19,32 @@ def open_all_files(directory):
                         names=["e", "nb", "Te", "Tnb", "mub_e", "mub_nb"],
                     )
 
-        if not all([press is not None, enerdens is not None, bardens is not None]):
-            raise ValueError("Missing required data files!")
-
-        return press, enerdens, bardens, inv
+        return inv
 
     except Exception as e:
         print(f"Error: {e}")
-        return None, None, None
+        return None
 
 
-directory = "/Users/tomaspolednicek/Desktop/EoS-script/data"  # Nastavte správnou cestu k souborům
-press, enerdens, bardens, inv = open_all_files(directory)
-# Convert pandas Series to NumPy arrays
+directory = "/Users/tomaspolednicek/Desktop/EoS-script/data"
+inv = open_all_files(directory)
 e = inv["e"].to_numpy()
 nb = inv["nb"].to_numpy()
-Te = inv["mub_e"].to_numpy()
+Te = inv["mub_nb"].to_numpy()
 
-# Create a 2D grid for interpolation
 E, NB = np.meshgrid(
     np.linspace(e.min(), e.max(), len(e)), np.linspace(nb.min(), nb.max(), len(e))
 )
 
-# Interpolate Te values to match the grid
-Te_grid = griddata((e, nb), Te, (E, NB), method="cubic")
+Te_grid = griddata((e, nb), Te, (E, NB), method="linear")
 
-# Create 3D plot
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection="3d")
 
-# Plot surface
-# ax.plot_surface(E, NB, Te_grid, cmap="viridis")
-ax.scatter(e, nb, Te, c="r", marker="o", label="Data Points")
-# Labels
+ax.plot_surface(E, NB, Te_grid, cmap="viridis")
+# ax.scatter(e, nb, Te, c="r", marker="o", label="Data Points")
 ax.set_xlabel("e")
 ax.set_ylabel("nb")
 ax.set_zlabel("mub_e")
-ax.set_title("3D Surface Plot of e, nb, and Te")
 
 plt.show()
